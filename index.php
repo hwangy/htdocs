775a94ea3b34c2859a2d7041076fa370f0a00c3b
Yeongwoo Hwang <yeongwoo@gmail.com>
2014-08-01 18:04:45 -0500

    CSS is hard

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php phpinfo(); ?>
+<html lang="en">
+<head>
+	<title>WOOBURGH</title>
+
+	<link rel="stylesheet" media="screen" href="style.css">
+</head>
+
+<body>
+	<section class="intro" role="banner">
+	<header>
+		<h1>WooBurgh</h1>
+		<h2>The Wonderous World of HTML/PHP/JAVASCRIPT</h2>
+	</header>
+	</section>
+</body>
+</html>
